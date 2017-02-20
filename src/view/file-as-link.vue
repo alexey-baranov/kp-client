7419f74ddd244e68358cbbfe0f4871837222e3a7
alexey_baranov <alexey_baranov@inbox.ru>
2017-02-20 13:21:56 +0500

    fix file-as-link.vue path to path+filter

--- src/view/file-as-link.vue
+++ src/view/file-as-link.vue
@@ -3,7 +3,7 @@
     <slot>
       <i class="material-icons md-dark md-1em">attachment</i>
     </slot>
-    <a :href="pathFromData" class="file-as-link" @click.stop="">
+    <a :href="model.path | download" class="file-as-link" @click.stop="">
       {{model.name}} ({{model.size | humanizeDiskSpase }})
     </a>
 
@@ -24,20 +24,16 @@
   export default  {
     mixins: [require("./mixin/humanize")],
     name: "kopnik-as-link",
-    data(){
-      /**
-       * замена computed.path который почему то не реактивится после загрузки файла
-       */
-      return {pathFromData: undefined}
-    },
     props: ["id", "model", "target"],
+    filters:{
+      download(value){
+        let result = `${config["file-server"].schema}://${config["file-server"].host}:${config["file-server"].port}/${config["file-server"]["download-path"]}`
+        result += `?path=${encodeURIComponent(value)}`
+        return result
+      }
+    },
     watch: {},
     computed: {
-      path(){
-        let result = `${config["file-server"].schema}://${config["file-server"].host}:${config["file-server"].port}/${config["file-server"]["download-path"]}`
-        result += `?path=${encodeURIComponent(this.model.path)}`
-        return result
-      },
       canDownload(){
         return this.model.path
       }
@@ -47,15 +43,6 @@
       this.log = require("loglevel").getLogger(this.$options.name + ".vue")
       if (this.model.id) {
         await this.model.joinedLoaded()
-        this.pathFromData = this.path
-      }
-      else {
-        let STUPID_BUG_PATH_INTERVAL = setInterval(() => {
-          if (this.model.path) {
-            this.pathFromData = this.path
-            clearInterval(STUPID_BUG_PATH_INTERVAL)
-          }
-        }, 1000)
       }
     }
   }
